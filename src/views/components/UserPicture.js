892dcb3193e496a816dd79d1c20a34be7dabf1c8
Lotfi <Lotfi@DESKTOP-S65O4MC>
2021-04-14 14:41:30 +0000

    update pictures

--- src/views/components/UserPicture.js
+++ src/views/components/UserPicture.js
@@ -1,30 +1,17 @@
 import React from "react";
-import { Buffer } from 'buffer'
 
-/*
-
-*/
 const UserPicture = ({ user, size, badge }) => {
-  const toBase64 = (input) => {
-    return Buffer.from(input, 'utf-8').toString('base64')
-  }
-  const test = () => {
-    console.log(user)
-  }
-  test()
   return (
     <div className="col-auto">
       {user.profilePicture != null && (
-        <div>
-          <span
-            className={`avatar avatar-${size}`}
-            style={{
-              backgroundImage: `url(data:${user.profilePicture.mimetype};base64,${toBase64(user.profilePicture.data)})`,
-            }}
-          >
-            {badge && <span className="badge bg-green"></span>}
-          </span>
-        </div>
+        <span
+          className={`avatar avatar-${size}`}
+          style={{
+            backgroundImage: `url(${process.env.REACT_APP_BACKEND_URL}/pictures/${user.profilePicture})`,
+          }}
+        >
+          {badge && <span className="badge bg-green"></span>}
+        </span>
       )}
       {user.profilePicture == null && (
         <span className={`bg-blue-lt avatar avatar-${size}`}>
